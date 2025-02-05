14ae1af0568da8dc28e833271889ddff1beaa70a
Marcel van den Dungen <marcel@van-den-dungen.com>
2025-02-04 23:15:21 -0800

    Resample sensor data on 30min interval, add md5 hash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from homeassistant import HomeAssistant, Sensor, Period
-from csv_writer import CsvWriter
 import hashlib
+import pandas as pd
 
 ha = HomeAssistant()
 
@@ -10,27 +10,51 @@ def calc_md5(args):
     return hashlib.md5(text.encode("utf-8")).hexdigest()
 
 
-with CsvWriter("temperature.csv") as writer:
+def resample_df(lst, id):
+    df = pd.DataFrame(lst)
 
-    # iterate over all temperature sensors
-    for sensor in ha.select(type=Sensor.TEMPERATURE):
-        # for each sensor, iterate over all measurements
-        devices = ha.select(type=Sensor.TEMPERATURE, id=sensor.entity_id)
-        print(devices)
-        device = devices[0]
+    # Convert timestamp to datetime object
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    # Convert temperature to number
+    df["temperature"] = pd.to_numeric(df["temperature"])
 
-        for measurement in device.history(Period.TODAY):
-            lst = [measurement.last_updated, measurement.state, measurement.entity_id]
-            # write MD5 hash, datetime, temp, id to CSV file
-            writer.write(calc_md5(lst), *lst)
+    # Set timestamp as index
+    df.set_index("timestamp", inplace=True)
 
-with CsvWriter("humidity.csv") as writer:
+    # Resample to 30-minute intervals and calculate mean
+    df_resampled = df.resample("30min").mean()
 
-    for sensor in ha.select(type=Sensor.HUMIDITY):
-        devices = ha.select(type=Sensor.HUMIDITY, id=sensor.entity_id)
-        print(devices)
-        device = devices[0]
+    # round to single decimal
+    df_resampled["temperature"] = df_resampled["temperature"].round(1)
 
-        for measurement in device.history(Period.TODAY):
-            lst = [measurement.last_updated, measurement.state, measurement.entity_id]
-            writer.write(calc_md5(lst), *lst)
+    # fill missing data with previous value
+    df_resampled.fillna(method="ffill")
+
+    # add md5 hash and id columns
+    df_resampled[["md5", "id"]] = df_resampled.apply(
+        lambda row: pd.Series(
+            [calc_md5([id, str(row.name), str(row.temperature)]), id]
+        ),
+        axis=1,
+    )
+
+    return df_resampled
+
+
+# iterate over all temperature sensors
+for sensor in ha.select(type=Sensor.TEMPERATURE):
+
+    lst = []
+    # for each sensor, iterate over all measurements
+    for measurement in sensor.history(Period.TODAY):
+        # skip unavailable measurements
+        if measurement.state != "unavailable":
+            lst.append(
+                {
+                    "timestamp": measurement.last_updated,
+                    "temperature": measurement.state,
+                }
+            )
+
+    df = resample_df(lst, sensor.entity_id)
+    df.to_csv(f"{sensor.entity_id}.csv")
